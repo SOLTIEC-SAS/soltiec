4a339737f2eab7ba164ca3b4723ed92cfd1eec9e
Santiago Ramírez Pérez <santiagoramirezperez96@gmail.com>
2026-01-22 15:05:49 -0500

    Botón de WhatsApp

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WhatsappButton } from './components/whatsapp-button/whatsapp-button';
 import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,
+    WhatsappButton, 
     Footer
   ],
   templateUrl: './app.html',
