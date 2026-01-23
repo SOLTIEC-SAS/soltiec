a394379a0d281621afb4f3e7ee71ba840171eb13
Santiago Ramírez Pérez <santiagoramirezperez96@gmail.com>
2026-01-23 16:10:01 -0500

    Navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { Navbar } from './components/navbar/navbar';
 import { WhatsappButton } from './components/whatsapp-button/whatsapp-button';
 import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,
+    Navbar,
     WhatsappButton, 
     Footer
   ],
--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  serviciosAbierto = false;
+  menuAbierto = false;
 
+  toggleServicios() {
+    this.serviciosAbierto = !this.serviciosAbierto;
+  }
+
+  toggleMenu() {
+    this.menuAbierto = !this.menuAbierto;
+  }
 }
