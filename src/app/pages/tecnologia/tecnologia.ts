bd98353ae6c0b9834f779af1ac62e5da8cddcd11
Santiago Ramírez Pérez <santiagoramirezperez96@gmail.com>
2026-02-02 15:55:05 -0500

    Servicio de tecnología

---
+++ src/app/pages/tecnologia/tecnologia.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tecnologia',
+  imports: [],
+  templateUrl: './tecnologia.html',
+  styleUrl: './tecnologia.css',
+})
+export class Tecnologia {
+
+}
