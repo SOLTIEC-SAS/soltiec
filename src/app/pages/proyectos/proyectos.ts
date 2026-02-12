34dd2c248369b0ecca008ca5d89a30793e2a4ebb
santiagoramirez10 <santiagoramirezperez96@gmail.com>
2026-02-12 14:43:18 -0500

    Proyectos

--- src/app/pages/proyectos/proyectos.ts
+++ src/app/pages/proyectos/proyectos.ts
@@ -25,10 +25,16 @@ export class Proyectos {
     { titulo: 'Adecuación cancha sintética', categoria: 'Construcción', ubicacion: 'Medellín, Antioquia', imagen: 'https://raw.githubusercontent.com/SOLTIEC-SAS/banco-imagenes/main/Construccion2.JPEG', atl: 'Adecuación cancha sintética' },
     { titulo: 'SSFV on-grid 44 kWp', categoria: 'Energía', ubicacion: 'Quibdó, Chocó', imagen: 'https://raw.githubusercontent.com/SOLTIEC-SAS/banco-imagenes/main/Energia3.JPG', alt: 'SSFV on-grid 44 kWp' },
     { titulo: 'Adecuación cancha arenilla', categoria: 'Construcción', ubicacion: 'Medellín, Antioquia', imagen: 'https://raw.githubusercontent.com/SOLTIEC-SAS/banco-imagenes/main/Construccion3.jpg', atl: 'Adecuación cancha arenilla' },
-    { titulo: '', categoria: 'Energía', ubicacion: '', imagen: 'https://raw.githubusercontent.com/SOLTIEC-SAS/banco-imagenes/main/Energia4.jpg', alt: '' },
+    { titulo: 'SSFV híbrido 15 kWp', categoria: 'Energía', ubicacion: 'Cali, Valle del Cauca', imagen: 'https://raw.githubusercontent.com/SOLTIEC-SAS/banco-imagenes/main/Energia4.jpg', alt: 'SSFV on-grid 15 kWp' },
     { titulo: 'Adecuación parque infantil', categoria: 'Construcción', ubicacion: 'Medellín, Antioquia', imagen: 'https://raw.githubusercontent.com/SOLTIEC-SAS/banco-imagenes/main/Construccion4.JPEG', atl: 'Adecuación parque infantil' },
     { titulo: 'SSFV on-grid 29,33 kWp', categoria: 'Energía', ubicacion: 'Cali, Valle del Cauca', imagen: 'https://raw.githubusercontent.com/SOLTIEC-SAS/banco-imagenes/main/Energia5.jpg', alt: 'SSFV on-grid 29,33 kWp' },
     { titulo: 'Cambio de fachada', categoria: 'Construcción', ubicacion: 'Medellín, Antioquia', imagen: 'https://raw.githubusercontent.com/SOLTIEC-SAS/banco-imagenes/main/Construccion5.JPEG', atl: 'Cambio de fachada' },
+    
+
+    
+    { titulo: 'SSFV on-grid 4,7 kWp', categoria: 'Energía', ubicacion: 'Cali, Valle del Cauca', imagen: 'https://raw.githubusercontent.com/SOLTIEC-SAS/banco-imagenes/main/Energia6.jpeg', alt: 'SSFV on-grid 4,7 kWp' },
+    { titulo: 'SSFV para Radioenlaces', categoria: 'Energía', ubicacion: 'Montelíbano, Córdoba', imagen: 'https://raw.githubusercontent.com/SOLTIEC-SAS/banco-imagenes/main/Energia7.jpg', alt: 'SSFV para Radioenlaces' },
+
   ];
   get proyectosFiltrados() {
 
