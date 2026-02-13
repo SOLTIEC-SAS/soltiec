3fc8dff2fb1a02b827afaa190632fcd64e9e988d
santiagoramirez10 <santiagoramirezperez96@gmail.com>
2026-02-13 16:29:35 -0500

    Cambio imagenes

--- src/app/pages/proyectos/proyectos.ts
+++ src/app/pages/proyectos/proyectos.ts
@@ -19,21 +19,21 @@ export class Proyectos {
   tabActiva = 'Todos';
 
   proyectos = [
-    { titulo: 'SSFV híbrido 8,05 kWp', categoria: 'Energía', ubicacion: 'Cali, Valle del Cauca', imagen: 'https://raw.githubusercontent.com/SOLTIEC-SAS/banco-imagenes/main/Energia1.jpg', alt: 'SSFV híbrido 8,05 kWp' },
-    { titulo: 'Construcción parque infantil', categoria: 'Construcción', ubicacion: 'Medellín, Antioquia', imagen: 'https://raw.githubusercontent.com/SOLTIEC-SAS/banco-imagenes/main/Construccion1.JPEG', alt: 'Adecuación parque infantil' },
-    { titulo: 'SSFV on-grid 20,34 kWp', categoria: 'Energía', ubicacion: 'Istmina, Chocó', imagen: 'https://raw.githubusercontent.com/SOLTIEC-SAS/banco-imagenes/main/Energia2.png', alt: 'SSFV on-grid 20,34 kWp' },
-    { titulo: 'Adecuación cancha sintética', categoria: 'Construcción', ubicacion: 'Medellín, Antioquia', imagen: 'https://raw.githubusercontent.com/SOLTIEC-SAS/banco-imagenes/main/Construccion2.JPEG', atl: 'Adecuación cancha sintética' },
-    { titulo: 'SSFV on-grid 44 kWp', categoria: 'Energía', ubicacion: 'Quibdó, Chocó', imagen: 'https://raw.githubusercontent.com/SOLTIEC-SAS/banco-imagenes/main/Energia3.JPG', alt: 'SSFV on-grid 44 kWp' },
-    { titulo: 'Adecuación cancha arenilla', categoria: 'Construcción', ubicacion: 'Medellín, Antioquia', imagen: 'https://raw.githubusercontent.com/SOLTIEC-SAS/banco-imagenes/main/Construccion3.jpg', atl: 'Adecuación cancha arenilla' },
-    { titulo: 'SSFV híbrido 15 kWp', categoria: 'Energía', ubicacion: 'Cali, Valle del Cauca', imagen: 'https://raw.githubusercontent.com/SOLTIEC-SAS/banco-imagenes/main/Energia4.jpg', alt: 'SSFV on-grid 15 kWp' },
-    { titulo: 'Adecuación parque infantil', categoria: 'Construcción', ubicacion: 'Medellín, Antioquia', imagen: 'https://raw.githubusercontent.com/SOLTIEC-SAS/banco-imagenes/main/Construccion4.JPEG', atl: 'Adecuación parque infantil' },
-    { titulo: 'SSFV on-grid 29,33 kWp', categoria: 'Energía', ubicacion: 'Cali, Valle del Cauca', imagen: 'https://raw.githubusercontent.com/SOLTIEC-SAS/banco-imagenes/main/Energia5.jpg', alt: 'SSFV on-grid 29,33 kWp' },
-    { titulo: 'Cambio de fachada', categoria: 'Construcción', ubicacion: 'Medellín, Antioquia', imagen: 'https://raw.githubusercontent.com/SOLTIEC-SAS/banco-imagenes/main/Construccion5.JPEG', atl: 'Cambio de fachada' },
+    { titulo: 'SSFV híbrido 8,05 kWp', categoria: 'Energía', ubicacion: 'Cali, Valle del Cauca', imagen: 'assets/images/Energia1.jpg', alt: 'SSFV híbrido 8,05 kWp' },
+    { titulo: 'Construcción parque infantil', categoria: 'Construcción', ubicacion: 'Medellín, Antioquia', imagen: 'assets/images/Construccion1.JPEG', alt: 'Adecuación parque infantil' },
+    { titulo: 'SSFV on-grid 20,34 kWp', categoria: 'Energía', ubicacion: 'Istmina, Chocó', imagen: 'assets/images/Energia2.png', alt: 'SSFV on-grid 20,34 kWp' },
+    { titulo: 'Adecuación cancha sintética', categoria: 'Construcción', ubicacion: 'Medellín, Antioquia', imagen: 'assets/images/Construccion2.JPEG', atl: 'Adecuación cancha sintética' },
+    { titulo: 'SSFV on-grid 44 kWp', categoria: 'Energía', ubicacion: 'Quibdó, Chocó', imagen: 'assets/images/Energia3.JPG', alt: 'SSFV on-grid 44 kWp' },
+    { titulo: 'Adecuación cancha arenilla', categoria: 'Construcción', ubicacion: 'Medellín, Antioquia', imagen: 'assets/images/Construccion3.jpg', atl: 'Adecuación cancha arenilla' },
+    { titulo: 'SSFV híbrido 15 kWp', categoria: 'Energía', ubicacion: 'Cali, Valle del Cauca', imagen: 'assets/images/Energia4.jpg', alt: 'SSFV on-grid 15 kWp' },
+    { titulo: 'Adecuación parque infantil', categoria: 'Construcción', ubicacion: 'Medellín, Antioquia', imagen: 'assets/images/Construccion4.JPEG', atl: 'Adecuación parque infantil' },
+    { titulo: 'SSFV on-grid 29,33 kWp', categoria: 'Energía', ubicacion: 'Cali, Valle del Cauca', imagen: 'assets/images/Energia5.jpg', alt: 'SSFV on-grid 29,33 kWp' },
+    { titulo: 'Cambio de fachada', categoria: 'Construcción', ubicacion: 'Medellín, Antioquia', imagen: 'assets/images/Construccion5.JPEG', atl: 'Cambio de fachada' },
     
 
     
-    { titulo: 'SSFV on-grid 4,7 kWp', categoria: 'Energía', ubicacion: 'Cali, Valle del Cauca', imagen: 'https://raw.githubusercontent.com/SOLTIEC-SAS/banco-imagenes/main/Energia6.jpeg', alt: 'SSFV on-grid 4,7 kWp' },
-    { titulo: 'SSFV para Radioenlaces', categoria: 'Energía', ubicacion: 'Montelíbano, Córdoba', imagen: 'https://raw.githubusercontent.com/SOLTIEC-SAS/banco-imagenes/main/Energia7.jpg', alt: 'SSFV para Radioenlaces' },
+    { titulo: 'SSFV on-grid 4,7 kWp', categoria: 'Energía', ubicacion: 'Cali, Valle del Cauca', imagen: 'assets/images/Energia6.jpeg', alt: 'SSFV on-grid 4,7 kWp' },
+    { titulo: 'SSFV para Radioenlaces', categoria: 'Energía', ubicacion: 'Montelíbano, Córdoba', imagen: 'assets/images/Energia7.jpg', alt: 'SSFV para Radioenlaces' },
 
   ];
   get proyectosFiltrados() {
